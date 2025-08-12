3706ed04591fbcbbdaf70369dbe4678dc35cbfaa
Margareths14 <Margarethsanchez762@gmail.com>
2025-08-12 14:14:16 +0000

    added css

--- script.js
+++ script.js
@@ -1,72 +1,71 @@
-const review=[
-    {
-        id: 1,
-        name:'Shelby Shoup',
-        job:'Middle school advisor/humanities teacher',
-        img:'Shelby shoup.jpg',
-        text:'Margareth is a wonderful student who is always eager to learn and grow. She was my advise from 6th to 8th grade, and I was consistently impressed by her dedication and her egerness to take on new challenges. She is a natural leader who is always willing to help her classmates. I have no doubt that she will continue to excel in all her future endeavors. I am so proud of her and cant wait to see all the amazing things she will accomplish in the future! '
-    }
-]
-
-
+const reviews = [
 {
-        id: 1,
-        name: 'Aminata Sako',
-        job: 'web developer',
-        img: '85099CF9-83D8-4B5B-8314-28424EFEF9CD.JPG',
-        text: 'Margareth is a dedicated and hardworking friend. She is always willing to lend a helping hand when she could and is always there to support her friends. She is a great listener and always has a positive attitude. Margareth is also very creative and has a great sense of humor. She is very passionate about her intrests and is always eager to try new things.'
-    }
+    id: 1,
+    name: 'Shelby Shoup',
+    job:'Humanities Teacher/middle school advisor',
+    img: 'shelby shoup.jpg',
+    text:'Margareth is a wonderful student who is always eager to learn and grow. She was my advise from 6th to 8th grade, and I was consistently impressed by her dedication and her egerness to take on new challenges. She is a natural leader who is always willing to help her classmates. I have no doubt that she will continue to excel in all everything she does. I am so proud of her and cant wait to see all the amazing things she will accomplish in the future!.',
+    
+},
+{
+    id: 2,
+    name: 'Aminata Sako',
+    job:'web developer',
+    img: '85099CF9-83D8-4B5B-8314-28424EFEF9CD.JPG',
+    text:'Margareth is a dedicated and hardworking friend. She is always willing to lend a helping hand when she can and is always there to support her friends. She is a great listener and always has a positive attitude. Margareth is also very creative and has a great sense of humor. She is very passionate about her intrests and is always eager to try new things.',
+    
+},
+{
+    id: 3,
+    name: 'Makayla Amoto',
+    job:'Web developer',
+    img: 'IMG_8166.jpeg',
+    text:'Margareth is a kind and caring person who always puts others first. She is a great friend who is always there to listen and support her friends. She is also very creative and has a great sense of humor. Margareth is very passionate about her interests and is always eager to learn new things. She is a great role model for her peers and is always willing to help others.',
+},
+];
 
-    {
-        id: 1,
-        name: 'peter jones',
-        job:'web developer'
-        img:'#'
-        text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iustoasperiores debitis incidunt, eius earum ipsam cupiditate libero?Iste, doloremque nihil?'
-    }
-]
-//section items
-  const img =document.getElementById('person-img');
-  const author= document.getElementById('author');
-  const job= document.getElementById('job');
-  const info= document.getElementById('info');
+//select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
 
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn'); // make sure HTML uses .prev-btn
+const nextBtn = document.querySelector('.next-btn'); // make sure HTML uses .next-btn
 
 let currentItem = 0;
 
-window.addEventListener('DOMContentLoaded', function() {
-    const item =review [currentItem];
-    img.src = item.img;
-    author.textContent = item.name;
-    job.textContent = item.job;
-    info.textContent = item.text;
+// load initial item
+window.addEventListener('DOMContentLoaded', function () {
+    showPerson(currentItem);
 });
 
 function showPerson(person) {
-    const item = review[person];
+    const item = reviews[person];
     img.src = item.img;
     author.textContent = item.name;
     job.textContent = item.job;
     info.textContent = item.text;
 }
 
-nextBtn.addEventListener('click', function() {
+// show next person
+nextBtn.addEventListener('click', function () {
     currentItem++;
-    if (currentItem > review.length - 1) {
+    if (currentItem > reviews.length - 1) {
         currentItem = 0;
     }
     showPerson(currentItem);
 });
 
-prevBtn.addEventListener('click', function() {
+// show previous person
+prevBtn.addEventListener('click', function () {
     currentItem--;
     if (currentItem < 0) {
         currentItem = reviews.length - 1;
     }
     showPerson(currentItem);
 });
+
 
 
 
